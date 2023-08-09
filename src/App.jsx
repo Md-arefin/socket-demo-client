b555dfbcf00a824e6e44f87392c983a243f166db
Md-arefin <ansariarefin617@gmail.com>
2023-08-09 16:40:13 +0600

    4 commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,51 @@
+import { useCallback, useState } from 'react'
 import './App.css'
 
 
 function App() {
 
+  // const [email, setEmail] = useState('');
+  // const [room, setRoom] = useState('');
+
+  const handleSubmit = useCallback((e) => {
+    e.preventDefault();
+
+    const form = e.target;
+    const email = form.email.value;
+    const room = form.room.value;
+
+    console.log(email, room)
+  }, [])
 
   return (
     <>
 
       <h1 className="lobby">Welcome to our Lobby</h1>
-      <form className='form'>
+
+      <form className='form' onSubmit={handleSubmit}>
+
         <h1 className="">Enter your email & room id</h1>
+
         <div>
+
           <label htmlFor="email">Email: </label>
-          <input type="email" id='email' required />
+
+          <input type="email" name='email' required />
+
         </div>
+
         <br />
+
         <div>
+
           <label htmlFor="room">Room id: </label>
-          <input type="text" id='room' required />
+
+          <input type="text" name='room' required />
+
         </div>
+
         <input className='join' type="submit" value="Join your meeting" />
+
       </form>
     </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import SocketProvide from './Provider/SocketProvide.jsx';
 
 const router = createBrowserRouter([
   {
@@ -16,6 +17,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <SocketProvide>
+      <RouterProvider router={router} />
+    </SocketProvide>
   </React.StrictMode>,
 )

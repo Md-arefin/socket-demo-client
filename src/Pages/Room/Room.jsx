4e34dbdde6b5cd2c212e99a12cdd1a92a19d57b4
Md-arefin <ansariarefin617@gmail.com>
2023-08-10 00:01:39 +0600

    7 commit

--- src/Pages/Room/Room.jsx
+++ src/Pages/Room/Room.jsx
@@ -1,6 +1,7 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import ReactPlayer from 'react-player';
 import { useSocket } from '../../Provider/SocketProvide';
+import Peer from '../Services/Peer';
 import './room.css';
 
 const Room = () => {
@@ -17,15 +18,29 @@ const Room = () => {
     }, [])
 
     const handleCallUser = useCallback(async () => {
-        const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
+        const stream = await navigator.mediaDevices.getUserMedia({
+            audio: true,
+            video: true,
+        });
+
+        const offer = await Peer.getOffer();
+        socket.emit('user:call', { to: remoteSocketId, offer});
+
         setMyStream(stream);
     }, [])
 
+    const handleIncomingCall = useCallback(({ from, offer }) =>{
+        console.log((`Incoming cal`, from , offer))
+    },[])
+
     useEffect(() => {
         socket.on('user:joined', handleUserJOined);
+          socket.on("incoming:call", handleIncomingCall);
 
         return () => {
             socket.off('user:joined', handleUserJOined);
+            socket.off("incoming:call", handleIncomingCall);
+
         }
     }, [])
     return (
@@ -35,16 +50,16 @@ const Room = () => {
                 remoteSocketId ? `${remoteSocketEmail} joined` : 'No one in the room'
             }</h4>
             <div className='center' >
-            {
-                remoteSocketId && <button onClick={handleCallUser}>Call</button>
-            }
+                {
+                    remoteSocketId && <button onClick={handleCallUser}>Call</button>
+                }
             </div>
             <div className='center-1'>
                 {
-                    myStream && <ReactPlayer 
+                    myStream && <ReactPlayer
                         url={myStream}
                         playing
-                        // muted
+                        muted
                         height={700}
                         width={700}
                     />
---
+++ src/Pages/Services/Peer.js
@@ -0,0 +1,27 @@
+class PeerService {
+    constructor() {
+        if (!this.peer) {
+            this.peer = new RTCPeerConnection({
+                iceServers: [
+                    {
+                        urls: [
+                            // google.com & twilio servers
+                            "stun:stun.l.google.com:19302",
+                            "stun:global.stun.twilio.com:3478",
+                        ]
+                    }
+                ]
+            })
+        }
+    }
+
+    async getOffer() {
+        if (this.peer) {
+            const offer = await this.peer.createOffer();
+            await this.peer.setLocalDescription(new RTCSessionDescription(offer));
+            return offer;
+        }
+    }
+}
+
+export default new PeerService();
